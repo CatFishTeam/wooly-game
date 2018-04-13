ef0cecf887b91e70c93bb6969c3d31c503fb63f0
guillaumepn <guillaumepn@free.fr>
2018-04-13 16:50:50 +0200

    fix triggers position + remove dark souls easter egg

--- src/functions/onDragEnd.js
+++ src/functions/onDragEnd.js
@@ -10,6 +10,7 @@ let triggerActions = main.triggerActions;
 module.exports = function onDragEnd() {
   if (this.picked) {
 
+    console.log(triggersObject);
     let onStep = false;
     let theStep;
     let theIndex;
@@ -19,6 +20,7 @@ module.exports = function onDragEnd() {
     if (this.data !== null && this.type === 'action') {
       for (let [index, step] of steps.children.entries()) {
         point = this.data.global;
+        console.log(point.x + ' ' + point.y);
         // Si on est au-dessus d'une case vide, on met l'action dessus
         if (point && step.getBounds().contains(point.x, point.y)) {
           if (stepsObject[index].type === 'empty' && (step.tint).toString(16) === 'ff00') {
@@ -32,12 +34,14 @@ module.exports = function onDragEnd() {
     } else if (this.data !== null && this.type === 'trigger') {
       for (let [index, trigger] of triggers.children.entries()) {
         let point = this.data.global;
+        console.log(point.x + ' ' + point.y);
         // Si on est au-dessus d'une case vide, on met l'action dessus
-        if (trigger.getBounds().contains(point.x, point.y)) {
+        if (trigger.containsPoint(point)) {
           if (triggersObject[index].type === 'empty' && trigger.alpha === 0.5) {
             theStep = trigger;
             theIndex = index;
             onStep = true;
+            console.log(theStep);
             break;
           }
         }
@@ -58,12 +62,25 @@ module.exports = function onDragEnd() {
       if (this.type === 'action') {
         stepsObject[theIndex].type = this.name;
         theStep.tint = 0xffffff;
+        this.x = theStep.getLocalBounds().x;
+        this.y = theStep.getLocalBounds().y;
       } else {
         triggersObject[theIndex].type = this.name;
         theStep.alpha = 1;
+        this.x = theStep.x;
+        this.y = theStep.y + 8;
       }
-      this.x = theStep.getLocalBounds().x;
-      this.y = theStep.getLocalBounds().y;
+      console.log('xy');
+      console.log(theStep.x);
+      console.log(theStep.y);
+      console.log('local bounds');
+      console.log(theStep.getLocalBounds().x);
+      console.log(theStep.getLocalBounds().y);
+      console.log('bounds');
+      console.log(theStep.getBounds().x);
+      console.log(theStep.getBounds().y);
+      console.log('global pos');
+      console.log(theStep.getGlobalPosition());
       this.onStep = true;
       this.currentStep = theIndex;
     } else {
